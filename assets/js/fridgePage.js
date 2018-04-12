0451c7696bb952f3964cc09739e33830ef2317e6
1605204 <teodor.narliyski@gmail.com>
2018-04-12 10:47:20 +0100

    test

--- assets/js/fridgePage.js
+++ assets/js/fridgePage.js
@@ -18,7 +18,23 @@ $( document ).ready(function() {
 })
 
 
-$("#scanbtn").click(function() {
-    Quagga.start();
-});
+Quagga.init({
+    inputStream : {
+      name : "Live",
+      type : "LiveStream",
+      target: document.querySelector('#scanbtn')    // Or '#yourElement' (optional)
+    },
+    decoder : {
+      readers : ["code_128_reader"]
+    }
+  }, function(err) {
+      if (err) {
+          console.log(err);
+          return
+      }
+      console.log("Initialization finished. Ready to start");
+      Quagga.start();
+  });
+
+
 });
