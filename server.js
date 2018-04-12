0ddc685d733b7c0f734f0a60ec3e9d50817fc4cf
Nona Todorova <n.g.todorova@rgu.ac.uk>
2018-04-12 12:53:26 +0100

    help

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
-var http = require('express');
+var express = require('express');
+var app = express();
 
-app.use(express.static('public'))
 
+app.get('add', function(req,res){
+  var x = req.query.x;
+  var y = req.query.y;
+  app.use(express.static('public'))
+
+})
 app.listen(8080);
