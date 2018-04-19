99851062d20e645651f53da5adca4b4fd69a3537
Nona Todorova <n.g.todorova@rgu.ac.uk>
2018-04-19 12:00:06 +0100

    HELP ME PLEASEEEEEEEEEEEEEEEEEE

--- server.js
+++ server.js
@@ -9,3 +9,41 @@ app.get('/add', function(req,res){
 
 })
 app.listen(8080);
+
+
+const MongoClient = require('mongodb').MongoClient;
+const url = "mongodb://localhost:27017/users";
+const express = require('express');
+const app = express();
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({extended:true}))
+var db;
+MongoClient.connect(url, function(err, database){
+ if(err) throw err;
+ db = database;
+ app.listen(8080);
+});
+
+app.get('/all', function(req, res) {
+ db.collection('users').find().toArray(function(err, result) {
+ if (err) throw err;
+ var output = "<h1>All the users</h1>";
+ for (var i = 0; i < result.length; i++) {
+ output += "<div>"
+ output += "<h3>" + result[i].name + "</h3>"
+ output += "<p>" + result[i].quote + "</p>"
+ output += "</div>"
+ }
+ res.send(output);
+ });
+});
+
+const bodyParser = require ('body-parser')
+
+app.post('/users', function (req, res) {
+ db.collection('users').save(req.body, function(err, result) {
+ if (err) throw err;
+ console.log('saved to database')
+ res.redirect('/')
+ })
+})
