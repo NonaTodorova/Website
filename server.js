60d11c9e2aa0e7f8477a576aef958dea1f9e9329
Nona Todorova <n.g.todorova@rgu.ac.uk>
2018-04-19 17:15:56 +0100

    test

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(bodyParser.urlencoded({extended:true}))
 
 
 
-app.use(express.static('public'));
+app.use(‘/public’, express.static(process.cwd() + ‘/public’));
 app.set('view engine', 'ejs');
 
 
