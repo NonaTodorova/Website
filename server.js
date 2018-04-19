1365a3b71741347011f58240005ecdf569c95f60
jozi99 <jozicricekt@hotmail.co.uk>
2018-04-19 18:08:37 +0100

    rendering

--- server.js
+++ server.js
@@ -8,40 +8,59 @@ const app = express();
 
 
 
-app.use(bodyParser.urlencoded({extended:true}))
+    app.use(bodyParser.urlencoded({extended:true}))
 // set the view engine to ejs
 
 
-app.set('view engine', 'ejs');
+  app.set('view engine', 'ejs');
 
-app.use(express.static('public'));
+  app.use(express.static('public'));
 
 
-app.get('/', function(req,res){
-  res.render('pages/home');
-})
-var db;
+  app.get('/home', function(req,res){
+    res.render('pages/home');
+  } )
 
-MongoClient.connect(url, function(err, database){
- if(err) throw err;
- db = database;
- app.listen(8080);
- console.log("listeneing");
-});
+  app.get('/fridge',function(req,res){
+    res.render('pages/fridge');
+  }
 
-app.get('/all', function(req, res) {
- db.collection('users').find().toArray(function(err, result) {
- if (err) throw err;
- var output = "<h1>All the users</h1>";
- for (var i = 0; i < result.length; i++) {
- output += "<div>"
- output += "<h3>" + result[i].name + "</h3>"
- output += "<p>" + result[i].quote + "</p>"
- output += "</div>"
- }
- res.send(output);
- });
-});
+  app.get('/login',function(req,res){
+    res.render('pages/login ');
+
+  }
+
+  app.get('/profilePage',function(req,res){
+    res.render('pages/profilePage');
+  }
+
+  app.get('register',function(req,res){
+    res.render('pages/register');
+  }
+
+
+  var db;
+
+  MongoClient.connect(url, function(err, database){
+    if(err) throw err;
+    db = database;
+    app.listen(8080);
+    console.log("listeneing");
+  });
+
+  app.get('/all', function(req, res) {
+      db.collection('users').find().toArray(function(err, result) {
+          if (err) throw err;
+            var output = "<h1>All the users</h1>";
+              for (var i = 0; i < result.length; i++) {
+                  output += "<div>"
+                    output += "<h3>" + result[i].name + "</h3>"
+                      output += "<p>" + result[i].quote + "</p>"
+                        output += "</div>"
+                        }
+                          res.send(output);
+                          });
+                        });
 
 
 
