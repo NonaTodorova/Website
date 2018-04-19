188586023b6d004f1a4dd6e0e603bc13ccf57e86
Nona Todorova <n.g.todorova@rgu.ac.uk>
2018-04-19 18:03:28 +0100

    omg

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 
 const MongoClient = require('mongodb').MongoClient; //npm install mongodb@2.2.32
-const url = "mongodb://localhost:27017/profiles";
+const url = "mongodb://localhost:27017/users";
 const express = require('express'); //npm install express
 const session = require('express-session'); //npm install express-session
 const bodyParser = require('body-parser'); //npm install body-parser
@@ -62,6 +62,6 @@ var user = {
  db.collection('users').save(user, function(err, result) {
  if (err) throw err;
  console.log('saved to database')
- res.redirect('/loginPage')
+ res.redirect('/login')
  })
 })
