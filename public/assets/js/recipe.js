b9e2d116d5e205a034b661eda56fe4eaae71d95e
jozi99 <jozicricekt@hotmail.co.uk>
2018-04-20 20:12:01 +0100

    g

--- public/assets/js/recipe.js
+++ public/assets/js/recipe.js
@@ -4,8 +4,8 @@ $(function(){
   var url = "https://api.edamam.com/search?q=chicken&app_id=107bd766&app_key=3bd0d97e2ed7c692c9cd9fef319ee6a4";
 
   $.getJSON(url,function(jsondata){
-    var myjson = JSON.stringify(jsondata);
-    console.log(myjson);
+    // var myjson = JSON.stringify(jsondata);
+    console.log(jsondata);
 
 });
 });
