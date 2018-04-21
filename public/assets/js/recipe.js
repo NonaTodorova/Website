7040f4600607358ba051280befb090c5290449cb
jozi99 <jozicricekt@hotmail.co.uk>
2018-04-21 22:14:39 +0100

    ff

--- public/assets/js/recipe.js
+++ public/assets/js/recipe.js
@@ -13,7 +13,7 @@ $(function(){
 
  //var myjson =  JSON.stringify(jsondata.hits[0].recipe.totalNutrients,null,4);
 
- var value = jsondata.hits[0].recipe.totalNutrients.Carbs;
+ var value = jsondata.hits[0].recipe.totalNutrients.FAT;
 
     console.log(value);
 
