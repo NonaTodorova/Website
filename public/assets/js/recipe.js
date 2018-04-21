08651c4f8a657258755509b15762ad51ffc92b1b
jozi99 <jozicricekt@hotmail.co.uk>
2018-04-21 19:43:03 +0100

    dd

--- public/assets/js/recipe.js
+++ public/assets/js/recipe.js
@@ -3,6 +3,8 @@ $(function(){
 
   var url = "https://api.edamam.com/search?q=Tropicana&app_id=107bd766&app_key=3bd0d97e2ed7c692c9cd9fef319ee6a4&totalNutrients";
 
+  var url2 = "";
+
 
 
   $.getJSON(url,function(jsondata){
