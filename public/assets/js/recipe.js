e370dc37bc2ba78821265995ec4683672ec7f941
jozi99 <jozicricekt@hotmail.co.uk>
2018-04-21 20:25:27 +0100

    dd

--- public/assets/js/recipe.js
+++ public/assets/js/recipe.js
@@ -9,7 +9,8 @@ $(function(){
 
   $.getJSON(url,function(jsondata){
 //   var myjson = JSON.stringify(jsondata);
-    console.log(jsondata);
+ var myJSON = JSON.stringify(jsondata);
+    console.log(myJSON);
 
 });
 });
