4fbbf44945eaee8a7598da2b6b01a677a49aa12b
jozi99 <jozicricekt@hotmail.co.uk>
2018-04-22 15:12:28 +0100

    G

--- public/assets/js/recipe.js
+++ public/assets/js/recipe.js
@@ -25,10 +25,10 @@ function getResultsForRecipes(jsondata){
 
 
 
-var output = "";
+
   for(var i = 0 ; i<jsondata.hits.length;i++){
   $('#scroll-bar').empty();
-
+var output = "";
 var card = "card";
     var image = jsondata.hits[i].recipe.image;
      var label = jsondata.hits[i].recipe.label;
@@ -68,7 +68,7 @@ var card = "card";
 
   }
 
-  $('#scroll-bar').html(output);
+  $('#scroll-bar').append("<li>hello</li>");
 
 
 
