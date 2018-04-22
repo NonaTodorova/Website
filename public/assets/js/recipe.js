e5f28c48335079fca6052028f42f8e6cf2be7f39
jozi99 <jozicricekt@hotmail.co.uk>
2018-04-22 15:21:44 +0100

    f

--- public/assets/js/recipe.js
+++ public/assets/js/recipe.js
@@ -58,7 +58,7 @@ var card = "card";
      output += '<div class= "card" >  '
      + '<img class = "card-img-top"   src=  "'+image+'"  > '
       + '<div class = "card-body" > '
-      + '<p> class = "card-text" > '+label+' </p> '
+      + '<p class = "card-text" > '+label+' </p> '
       +  '</div>'
       + '</div>';
 
