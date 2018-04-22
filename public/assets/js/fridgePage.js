76664c786e35828e846fb6cae0d2a39085a66838
teodor20 <teodor.narliyski@gmail.com>
2018-04-22 17:47:53 +0100

    asd

--- public/assets/js/fridgePage.js
+++ public/assets/js/fridgePage.js
@@ -90,8 +90,7 @@ function startScanner() {
 
 
     Quagga.onDetected(function (result) {
-      getDescription(result.codeResult.code);
-        alert("Barcode detected and processed : [" + result.codeResult.code + "]", result);
+        // alert("Barcode detected and processed : [" + result.codeResult.code + "]", result);
         Quagga.stop();
         _scannerIsRunning=false;
           $("#scanner-container").hide();
@@ -114,27 +113,3 @@ document.getElementById("btnScan").addEventListener("click", function () {
 }, false);
 
 });
-
-
-function getDescription(upc_code){
-
-  var url_test ="https://dev.tescolabs.com/product/?gtin=" + upc_code;
-
-$.ajax({
-           url: url_test,
-           beforeSend: function(xhrObj){
-               // Request headers
-               xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key","f4b09768d9a04ed198d32676e660526c");
-           },
-           type: "GET",
-           // Request body
-           data: "{body}",
-       })
-       .done(function(data) {
-      alert(data.products[0].description);
-           alert("success");
-       })
-       .fail(function() {
-           alert("error");
-       });
-   };
