da86b30e4629fcb77c0381826ba4eaa0a0e65bf3
jozi99 <jozicricekt@hotmail.co.uk>
2018-04-23 00:35:46 +0100

    d

--- public/assets/js/recipe.js
+++ public/assets/js/recipe.js
@@ -4,6 +4,8 @@
 
 $(function() {
 
+
+
   var url = "https://api.edamam.com/search?q=Chicken&app_id=107bd766&app_key=3bd0d97e2ed7c692c9cd9fef319ee6a4&from=0&to=5";
   $.getJSON(url,function(jsondata){
 
