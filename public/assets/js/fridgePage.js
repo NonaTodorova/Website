cab06ace52ab3cc55d514690736f225490534363
teodor20 <teodor.narliyski@gmail.com>
2018-04-22 22:45:09 +0100

    asd

--- public/assets/js/fridgePage.js
+++ public/assets/js/fridgePage.js
@@ -113,7 +113,7 @@ document.getElementById("btnScan").addEventListener("click", function () {
         $("#scanner-container").hide();
     } else {
         startScanner();
-        // _scannerIsRunning=true;
+        _scannerIsRunning=true;
         $("#scanner-container").show();
     }
 }, false);
