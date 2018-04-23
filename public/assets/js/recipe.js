efde791c305deeb22e6304156c4bde61365d7fa5
jozi99 <jozicricekt@hotmail.co.uk>
2018-04-23 10:57:49 +0100

    d

--- public/assets/js/recipe.js
+++ public/assets/js/recipe.js
@@ -1,14 +1,14 @@
 
-function myFunction(){
-  var default_Values = ["Wings","Trifle","Cheese"];
-
-  for(var i = 0 ; i<default_Values.legnth;i++){
-
-    getRecipesResults(default_Values[i]);
-
-  }
-
-  }
+// function myFunction(){
+//   var default_Values = ["Wings","Trifle","Cheese"];
+//
+//   for(var i = 0 ; i<default_Values.legnth;i++){
+//
+//     getRecipesResults(default_Values[i]);
+//
+//   }
+//
+//   }
 
 
     $('#searchform').submit(function(){
@@ -24,7 +24,7 @@ function myFunction(){
 
   function getRecipesResults(searchTerms){
 
-alert("hello");
+// alert("hello");
 
   var url = "https://api.edamam.com/search?q="+searchTerms+ "&app_id=107bd766&app_key=3bd0d97e2ed7c692c9cd9fef319ee6a4&from=0&to=5";
   $.getJSON(url,function(jsondata){
