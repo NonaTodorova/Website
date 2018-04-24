9993396dff997a7618d59a322d6893dbcd16d33f
jozi99 <jozicricekt@hotmail.co.uk>
2018-04-24 15:47:17 +0100

    f

--- public/assets/js/recipe.js
+++ public/assets/js/recipe.js
@@ -1,15 +1,15 @@
 
 
-// $(document).ready(function(){
-//
-//   var url = "https://api.edamam.com/search?q="+"Chicken" + "&app_id=107bd766&app_key=3bd0d97e2ed7c692c9cd9fef319ee6a4&from=0&to=5";
-//   $.getJSON(url,function(jsondata){
-//
-// getResultsForRecipes(jsondata);
-// //   var string = JSON.stringify(jsondata,null,4)
-// // console.log(string);
-// });
-// });
+$(document).ready(function(){
+
+  var url = "https://api.edamam.com/search?q="+"Chicken" + "&app_id=107bd766&app_key=3bd0d97e2ed7c692c9cd9fef319ee6a4&from=0&to=5";
+  $.getJSON(url,function(jsondata){
+
+getResultsForRecipes(jsondata);
+//   var string = JSON.stringify(jsondata,null,4)
+// console.log(string);
+});
+});
 
 
 $(function(){
