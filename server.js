8e13cbc62b58b8b3700b3ab54f1a51634e4c1054
jozi99 <jozicricekt@hotmail.co.uk>
2018-04-24 17:46:59 +0100

    f

--- server.js
+++ server.js
@@ -140,22 +140,22 @@ app.post('/loggingIn', function(req,res){
 });
 
 
-app.get('/addItem',function(req,res){
- var pull_Item = req.query.desc;
-// console.log(pull_Item);
-
-  db.collection('people').findOne({"email":req.session.user.email},function(err,result){
-if (err) throw err;
-
-db.collection('people').update({"items":[pull_Item]},function(err,result){
-console.log("Hello");
-if (err) throw err;
-})
-
-})
-
-
-})
+// app.get('/addItem',function(req,res){
+//  var pull_Item = req.query.desc;
+// // console.log(pull_Item);
+//
+//   db.collection('people').findOne({"email":req.session.user.email},function(err,result){
+// if (err) throw err;
+//
+// db.collection('people').update({"items":[pull_Item]},function(err,result){
+// console.log("Hello");
+// if (err) throw err;
+// })
+//
+// })
+//
+//
+// })
 
 
 
