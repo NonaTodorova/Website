df3fa21f2d44c0871bf525ec375c320468c24f03
1605204 <teodor.narliyski@gmail.com>
2018-04-26 15:36:55 +0100

    asd

--- server.js
+++ server.js
@@ -103,7 +103,7 @@ var person = {
   password: req.body.password,
   name : req.body.name,
   items:["Beef", "Chicken", "Fish"],
-  dates:[]
+  dates:["1"]
 }
 
 db.collection("people").save(person, function(err,result){
