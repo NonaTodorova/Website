2ffc5c4e9f115f52b9f37faba9f2b66a98cec414
jozi99 <jozicricekt@hotmail.co.uk>
2018-04-26 12:41:25 +0100

    r

--- server.js
+++ server.js
@@ -161,18 +161,18 @@ db.collection('people').update(
 
 })
 
-// TEST
-// app.get('/delete',function(req,res){
-//   db.collection('people').remove(
-//
-//      { email: req.session.user.email },
-//      { $pull:
-//
-//           {items:req.query.item}
-//         })
-//
-// })
-// TEST
+
+app.get('/delete',function(req,res){
+  db.collection('people').update(
+
+     { email: req.session.user.email },
+     { $pull:
+
+          {items:req.query.item}
+        })
+
+})
+
 
 
 
