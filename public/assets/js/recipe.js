3eeeb50454e41b195b7d6e089b8dc59d7ed98e67
jozi99 <jozicricekt@hotmail.co.uk>
2018-04-27 21:59:09 +0100

    d

--- public/assets/js/recipe.js
+++ public/assets/js/recipe.js
@@ -92,7 +92,7 @@ var card = "card";
 
       + '<div class="container"  width="50">'
       + '<img class = "card-img-top"   src=  "'+image+'"  >'
-      + '   <button type="button" class="btn btn-primary">View Recipe</button>'
+      + '   <button type="button" class="btn btn-danger">View Recipe</button>'
       + '</div>'
       + '<div class = "card-body" > '
       + '<p class = "card-text" > '+label+' </p> '
